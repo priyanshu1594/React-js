c444c8f257c4ebf006b61e11caf60410e591ebd6
isha.empyreal <isha.empyreal@gmail.com>
2025-02-12 18:51:30 +0530

    first commit

---
+++ src/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const About = () => {
+    return (
+        <div className="about">
+            <h1 className="title">About</h1>
+            <p className="text">This is a paragraph about the project.</p>
+        </div>
+    );
+
+}
+export default About;
---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import About from './About';
+import Car1 from './Car1';
+
+function App() {
+  return (
+    
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   <About></About>
+    //   </header>
+    // </div>
+    <div>
+    <Car1></Car1>
+    </div>
+  );
+}
+
+export default 
+
+
+
+Car1;
